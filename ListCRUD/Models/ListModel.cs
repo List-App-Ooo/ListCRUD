683f2abdc1c5206334054f1d114e3086d1f499ca
Daniel Hopson <hdhopson88@yahoo.com>
2020-12-30 13:39:28 -0800

    Creates List skeleton and model

---
+++ ListCRUD/Models/ListModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ListCRUD.Models
+{
+    public class ListModel<T>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int TotalItems { get; set; }
+        public List<T> Items { get; set; }
+    }
+}
