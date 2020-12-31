a38a8884b764b8f7072862565c8475238be9537e
Daniel Hopson <hdhopson88@yahoo.com>
2020-12-31 09:35:12 -0800

    Scopes list repo

--- ListCRUD/Startup.cs
+++ ListCRUD/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ListCRUD.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,7 @@ namespace ListCRUD
         {
 
             services.AddControllers();
+            services.AddScoped<IListRepo, ListRepo>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ListCRUD", Version = "v1" });
