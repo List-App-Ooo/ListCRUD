a39e9aa3e95b86a397e50b82782270ce2d2657b1
Daniel Hopson <hdhopson88@yahoo.com>
2021-01-07 21:09:10 -0800

    Updates list model

--- ListCRUD/Models/ListModel.cs
+++ ListCRUD/Models/ListModel.cs
@@ -6,8 +6,8 @@ namespace ListCRUD.Models
     public class ListModel
     {
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
         public string Title { get; set; }
         public int TotalItems { get; set; }
+        public Guid UserId { get; set; }
     }
 }
