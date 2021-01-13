c51237a1c0ea43b89ccfae09b7abf4f12a9a6839
Daniel Hopson <hdhopson88@yahoo.com>
2021-01-12 19:04:36 -0800

    Edits seed data

--- ListCRUD/Data/DbInitializer.cs
+++ ListCRUD/Data/DbInitializer.cs
@@ -19,12 +19,12 @@ namespace ListCRUD.Data
             var noteList = new ListModel();
             noteList.Id = new Guid("ef6a7925-80a6-44bc-9e03-89bc7ca3586e");
             noteList.Title = "Notes";
-            noteList.TotalItems = 3;
+            noteList.TotalItems = 0;
             
             var todoList = new ListModel();
             todoList.Id = new Guid("ef6a7925-80a6-44bc-9e03-89bc7ca3586f");
             todoList.Title = "Reminders";
-            todoList.TotalItems = 3;
+            todoList.TotalItems = 0;
 
             context.Lists.AddRange(noteList);
             context.Lists.AddRange(todoList);
